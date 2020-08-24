e6db8127e759bb41f26b47d33832e1c2a06f52b2
McEwan <romcewan@f8ffc202c4fc.ant.amazon.com>
2020-08-24 08:29:38 +0200

    bearer token

--- src/agent.js
+++ src/agent.js
@@ -12,7 +12,7 @@ const responseBody = res => res.body;
 let token = null;
 const tokenPlugin = req => {
   if (token) {
-    req.set('authorization', `Token ${token}`);
+    req.set('authorization', `Bearer ${token}`);
   }
 }
 
