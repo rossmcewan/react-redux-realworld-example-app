0909cd1db6c100ffbe9c27d04dc01d82e07181d2
McEwan <romcewan@f8ffc202c4fc.ant.amazon.com>
2020-08-25 22:40:54 +0200

    changed the URLs

--- src/agent.js
+++ src/agent.js
@@ -6,6 +6,10 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const API_ROOT = "https://oktank-backend.herokuapp.com/api";
 const USER_API_ROOT =
   "https://3cthybyobe.execute-api.us-east-1.amazonaws.com/api";
+const ARTICLE_API_ROOT =
+  "https://6arryo2p4h.execute-api.us-east-1.amazonaws.com/api";
+const PROFILE_API_ROOT =
+  "https://6arryo2p4h.execute-api.us-east-1.amazonaws.com/api";
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
@@ -41,41 +45,41 @@ const Auth = {
 };
 
 const Tags = {
-  getAll: () => requests.get()("/tags"),
+  getAll: () => requests.get(ARTICLE_API_ROOT)("/tags"),
 };
 
 const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
 const omitSlug = (article) => Object.assign({}, article, { slug: undefined });
 const Articles = {
-  all: (page) => requests.get()(`/articles?${limit(10, page)}`),
+  all: (page) => requests.get(ARTICLE_API_ROOT)(`/articles?${limit(10, page)}`),
   byAuthor: (author, page) =>
-    requests.get()(`/articles?author=${encode(author)}&${limit(5, page)}`),
+    requests.get(ARTICLE_API_ROOT)(`/articles?author=${encode(author)}&${limit(5, page)}`),
   byTag: (tag, page) =>
-    requests.get()(`/articles?tag=${encode(tag)}&${limit(10, page)}`),
-  del: (slug) => requests.del()(`/articles/${slug}`),
-  favorite: (slug) => requests.post()(`/articles/${slug}/favorite`),
+    requests.get(ARTICLE_API_ROOT)(`/articles?tag=${encode(tag)}&${limit(10, page)}`),
+  del: (slug) => requests.del(ARTICLE_API_ROOT)(`/articles/${slug}`),
+  favorite: (slug) => requests.post(ARTICLE_API_ROOT)(`/articles/${slug}/favorite`),
   favoritedBy: (author, page) =>
-    requests.get()(`/articles?favorited=${encode(author)}&${limit(5, page)}`),
-  feed: () => requests.get()("/articles/feed?limit=10&offset=0"),
-  get: (slug) => requests.get()(`/articles/${slug}`),
-  unfavorite: (slug) => requests.del()(`/articles/${slug}/favorite`),
+    requests.get(ARTICLE_API_ROOT)(`/articles?favorited=${encode(author)}&${limit(5, page)}`),
+  feed: () => requests.get(ARTICLE_API_ROOT)("/articles/feed?limit=10&offset=0"),
+  get: (slug) => requests.get(ARTICLE_API_ROOT)(`/articles/${slug}`),
+  unfavorite: (slug) => requests.del(ARTICLE_API_ROOT)(`/articles/${slug}/favorite`),
   update: (article) =>
-    requests.put()(`/articles/${article.slug}`, { article: omitSlug(article) }),
-  create: (article) => requests.post()("/articles", { article }),
+    requests.put(ARTICLE_API_ROOT)(`/articles/${article.slug}`, { article: omitSlug(article) }),
+  create: (article) => requests.post(ARTICLE_API_ROOT)("/articles", { article }),
 };
 
 const Comments = {
   create: (slug, comment) =>
-    requests.post()(`/articles/${slug}/comments`, { comment }),
+    requests.post(ARTICLE_API_ROOT)(`/articles/${slug}/comments`, { comment }),
   delete: (slug, commentId) =>
-    requests.del()(`/articles/${slug}/comments/${commentId}`),
+    requests.del(ARTICLE_API_ROOT)(`/articles/${slug}/comments/${commentId}`),
   forArticle: (slug) => requests.get()(`/articles/${slug}/comments`),
 };
 
 const Profile = {
-  follow: (username) => requests.post()(`/profiles/${username}/follow`),
-  get: (username) => requests.get()(`/profiles/${username}`),
-  unfollow: (username) => requests.del()(`/profiles/${username}/follow`),
+  follow: (username) => requests.post(ARTICLE_API_ROOT)(`/profiles/${username}/follow`),
+  get: (username) => requests.get(ARTICLE_API_ROOT)(`/profiles/${username}`),
+  unfollow: (username) => requests.del(ARTICLE_API_ROOT)(`/profiles/${username}/follow`),
 };
 
 export default {
